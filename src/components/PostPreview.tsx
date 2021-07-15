d811777709731666df8bd30974fb839b269ca6b8
Aditya Pratap <adityapratap7344@gmail.com>
2021-07-16 03:06:50 +0530

    add post preview UI and rendering posts

---
+++ src/components/PostPreview.tsx
@@ -0,0 +1,108 @@
+import {
+  Card,
+  CardContent,
+  // CardMedia,
+  createStyles,
+  Grid,
+  IconButton,
+  makeStyles,
+  Typography,
+  useMediaQuery,
+} from "@material-ui/core";
+import ArrowUpIcon from "@material-ui/icons/ArrowUpwardRounded";
+import ArrowDownIcon from "@material-ui/icons/ArrowDownwardRounded";
+import { Post } from "../API";
+
+interface Props {
+  post: Post;
+}
+
+const PostPreview = ({ post }: Props) => {
+  const matches = useMediaQuery("(max-width: 960px)");
+  const classes = useStyles({ matches })();
+
+  return (
+    <Card elevation={3}>
+      <CardContent>
+        <Grid container spacing={2} className={classes.mainGrid}>
+          <Grid
+            container
+            spacing={2}
+            item
+            sm={12}
+            md={1}
+            className={classes.votes}
+          >
+            <Grid item sm={2} md={12}>
+              <IconButton color="primary" size="small">
+                <ArrowUpIcon className={classes.voteBtnIcon} />
+              </IconButton>
+            </Grid>
+            <Grid item sm={2} md={12}>
+              votes
+            </Grid>
+            <Grid item sm={2} md={12}>
+              <IconButton color="primary" size="small">
+                <ArrowDownIcon className={classes.voteBtnIcon} />
+              </IconButton>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            item
+            sm={12}
+            md={11}
+            spacing={2}
+            className={classes.content}
+          >
+            <Grid item xs={12}>
+              <Typography variant="body1">
+                Posted by <b>{post.owner}</b> at {post.createdAt}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h2">{post.title}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              {/* <CardMedia
+                image="https://cdn.mos.cms.futurecdn.net/LBjdeVmH2C7KYspZBFH2hU.jpg"
+                title="iMac 24 Purple"
+                className={classes.picture}
+              /> */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default PostPreview;
+
+interface UseStyleProps {
+  matches: boolean;
+}
+
+const useStyles = (props: UseStyleProps) =>
+  makeStyles((theme) =>
+    createStyles({
+      mainGrid: {
+        flexGrow: 1,
+        flexDirection: props.matches ? "column-reverse" : "row",
+      },
+      votes: {
+        height: "100%",
+        width: "100%",
+        flexDirection: props.matches ? "row" : "column",
+      },
+      voteBtnIcon: {
+        fontSize: 30,
+      },
+      content: {},
+      picture: {
+        width: "100%",
+        height: "30rem",
+        objectFit: "cover",
+      },
+    })
+  );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,65 @@
-import { Typography } from "@material-ui/core";
+import { createStyles, makeStyles, Container, Grid } from "@material-ui/core";
+import { API } from "aws-amplify";
+import { useEffect, useState } from "react";
+import { ListPostsQuery, Post } from "../API";
+import PostPreview from "../components/PostPreview";
 import { useUser } from "../context/AuthContext";
+import { listPosts } from "../graphql/queries";
 
 export default function Home() {
+  const classes = useStyles();
   const { user } = useUser();
-  console.log("USER: ", user);
-  return <Typography variant="h1">Hello {user?.getUsername()}</Typography>;
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    const fetchPostsFromApi = async () => {
+      const allPosts = (await API.graphql({ query: listPosts })) as {
+        data: ListPostsQuery;
+        errors: { errorType: string; message: string; errorInfo: any }[];
+      };
+
+      if (allPosts.data) {
+        setPosts(allPosts.data.listPosts.items);
+      } else {
+        setError("Couldn't get posts");
+        console.log(allPosts.errors);
+      }
+    };
+
+    fetchPostsFromApi();
+  }, []);
+
+  useEffect(() => {
+    console.log("USER: ", user);
+  }, [user]);
+
+  useEffect(() => {
+    console.log("POSTS: ", posts);
+  }, [posts]);
+
+  useEffect(() => {
+    console.log("ERROR: ", error);
+  }, [error]);
+
+  return (
+    <Container maxWidth="md" className={classes.root}>
+      <Grid container spacing={3}>
+        {posts.map((post) => (
+          <Grid item key={post.id} xs={12}>
+            <PostPreview post={post} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      padding: theme.spacing(2),
+    },
+  })
+);
